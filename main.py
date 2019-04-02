8105dda249312c473452087634299c08e0699b39
Oksana Yatskiv <oksanayatskiv89@gmail.com>
2019-04-02 10:31:51 -0500

    processing the form

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask,request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG']= True
+
+form = """
+        <!DOCTYPE html>
+
+        <html>
+            <head>
+                <style>
+                    form {
+                        background-color: #eee;
+                        padding: 20px;
+                        margin: 0 auto;
+                        width: 540px;
+                        font: 16px sans-serif;
+                        border-radius: 10px;
+                    }
+                    textarea {
+                        margin: 10px 0;
+                        width: 540px;
+                        height: 120px;
+                    }
+                </style>
+            </head>
+            <body>
+            <!-- create your form here -->
+                <form method="POST">
+                    <label for = "rot">Rotate by:</label>
+                    <input type = "text" id = "rot" value= 0 name = "rot"/>
+                    <textarea name="text"></textarea> 
+                    <input type="submit"/>
+                </form>
+            </body>
+        </html>
+        """
+
+@app.route("/")
+def index():
+    return form
+app.run()
+
+@app.route('/encrypt', methods=['POST'])
+def encrypt():
+    rotation = request.form['rot']
+    text = request.form['text']
+    return '<h1>Hello, ' + #should be putted + '</h1>'
